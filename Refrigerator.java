5c0453cb83cb6526df6440ff86b2abfaf4874a1a
tafannummysha-del <tafannummysha@gmail.com>
2026-08-18 12:52:02 +0800

    Delete Refrigerator.java

--- Refrigerator.java
+++
@@ -1,9 +0,0 @@
-class Refrigerator  extends Appliance {
-    public Refrigerator (String brand) {
-        super (brand);
-    }
-    @Override
-    public void operate () {
-        System.out.println("Store food & beverages...");
-    }
-}
